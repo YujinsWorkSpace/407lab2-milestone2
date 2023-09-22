3357493ddd46d8ea1a6f16e9738c62881438df9c
Yujin Wang <ywang2589@wisc.edu>
2023-09-22 00:13:41 -0500

    update calculator operation

--- app/src/main/java/com/cs407/calculator/MainActivity.java
+++ app/src/main/java/com/cs407/calculator/MainActivity.java
@@ -3,6 +3,10 @@ package com.cs407.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,57 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void addition(int i, int j){
+        float result = i + j;
+        Toast.makeText(this, "Result: " + result, Toast.LENGTH_SHORT).show();
+    }
+
+    public void substraction(int i, int j){
+        int result = i - j;
+        Toast.makeText(this, "Result: " + result, Toast.LENGTH_SHORT).show();
+    }
+
+    public void multiplication(int i, int j){
+        int result = i * j;
+        Toast.makeText(this, "Result: " + result, Toast.LENGTH_SHORT).show();
+    }
+
+    public void division(int i, int j){
+        float result = (float) i / (float) j;
+        Toast.makeText(this, "Result: " + result, Toast.LENGTH_SHORT).show();
+    }
+
+    public void Onclick1(View view){
+        EditText num1 = (EditText) findViewById(R.id.firstNum);
+        EditText num2 = (EditText) findViewById(R.id.secondNum);
+        int firstNum = Integer.parseInt(num1.getText().toString());
+        int secondNum = Integer.parseInt(num2.getText().toString());
+        addition(firstNum, secondNum);
+    }
+
+    public void Onclick2(View view){
+        EditText num1 = (EditText) findViewById(R.id.firstNum);
+        EditText num2 = (EditText) findViewById(R.id.secondNum);
+        int firstNum = Integer.parseInt(num1.getText().toString());
+        int secondNum = Integer.parseInt(num2.getText().toString());
+        substraction(firstNum, secondNum);
+    }
+
+    public void Onclick3(View view){
+        EditText num1 = (EditText) findViewById(R.id.firstNum);
+        EditText num2 = (EditText) findViewById(R.id.secondNum);
+        int firstNum = Integer.parseInt(num1.getText().toString());
+        int secondNum = Integer.parseInt(num2.getText().toString());
+        multiplication(firstNum, secondNum);
+    }
+
+    public void Onclick4(View view){
+        EditText num1 = (EditText) findViewById(R.id.firstNum);
+        EditText num2 = (EditText) findViewById(R.id.secondNum);
+        int firstNum = Integer.parseInt(num1.getText().toString());
+        int secondNum = Integer.parseInt(num2.getText().toString());
+        division(firstNum, secondNum);
     }
 }
